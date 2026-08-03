58c703f0f71fb2f8654a3d71247507a6d68bdbb8
gabrielmcrvg <gabmercon@gmail.com>
2026-08-03 19:32:50 -0300

    Feat: Implementando modelos

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 
 alunos = [
-{'id': 1, 'nome': 'Gabriel', 'ativo': True},
-{'id': 2, 'nome': 'Joao', 'ativo': True},
-{'id': 3, 'nome': 'Pedro', 'ativo': False}
+{'id': 1, 'nome': 'Gabriel','idade': 23, 'ativo': True},
+{'id': 2, 'nome': 'Joao','idade': 25, 'ativo': False},
+{'id': 3, 'nome': 'Pedro','idade': 29, 'ativo': True},
 ]
 
 app = FastAPI()
+
+class AlunoEntrada(BaseModel):
+    nome:str = Field(min_length=3)
+    idade:int = Field(ge=16)
+    ativo: bool = True
+
+class AlunoResposta(BaseModel):
+    id: int
+    nome: str
+    idade: int
+    ativo: bool
+
+class AlunoPatch(BaseModel):
+    nome: str | None = Field(default=None, min_length=3)
+    idade: int | None = Field(default=None, ge=16)
+    ativo: bool | None = None
+
+# =-= GET =-=
 
 @app.get('/') # quando chegar um .get na rota (''/'')...
 
@@ -18,7 +37,7 @@ def raiz(): # ...me devolva um dicionario
 def status():
     return {'status': 'OK', 'Versão': '1.0'}
 
-@app.get('/alunos')
+@app.get('/alunos', response_model=list[AlunoResposta])
 
 def listar_alunos(ativo:bool | None = None, limite:int=10):
     resultado = alunos
@@ -26,7 +45,7 @@ def listar_alunos(ativo:bool | None = None, limite:int=10):
         resultado = [a for a in resultado if a['ativo'] == ativo]
     return resultado[:limite]
 
-@app.get('/alunos/{aluno_id}')
+@app.get('/alunos/{aluno_id}', response_model=AlunoResposta)
 def buscar_aluno(aluno_id:int):
     for aluno in alunos:
         if aluno['id'] == aluno_id:
@@ -36,28 +55,32 @@ def buscar_aluno(aluno_id:int):
 # =-= POST =-=
 
 @app.post('/alunos', status_code=201)
-def criar_aluno(aluno: dict):
-    alunos.append(aluno)
-    return aluno
+def criar_aluno(aluno: AlunoEntrada):
+    novo = aluno.model_dump()
+    novo['id'] = max([a['id'] for a in alunos], default=0) + 1
+    alunos.append(novo)
+    return novo
 
 # =-= PUT =-=
 
 @app.put('/alunos/{aluno_id}')
-def atualizar_aluno(aluno_id: int, dados: dict):
-    for indice, aluno in enumerate(alunos):
+def atualizar_aluno(aluno_id: int, dados: AlunoEntrada):
+    for indice, aluno in enumerate(alunos): # indice = posição, aluno = dicionario
         if aluno['id'] == aluno_id:
-            dados['id'] = aluno_id
-            alunos[indice] = dados
-            return dados
+            atualizado = dados.model_dump()
+            atualizado['id'] = aluno_id
+            alunos[indice] = atualizado
+            return atualizado
     raise HTTPException(status_code=404, detail='Aluno não encontrado!')
 
 # =-= PATCH =-=
 
 @app.patch('/alunos/{aluno_id}')
-def atualizar_aluno(aluno_id: int, dados: dict):
-    for indice, aluno in enumerate(alunos):
+def alterar_aluno(aluno_id: int, dados: AlunoPatch):
+    for aluno in alunos:
         if aluno['id'] == aluno_id:
-            aluno.update(dados)
+            mudancas = dados.model_dump(exclude_unset=True) # exclude unset só mexe no que eu pedi pra mexer, ignora o resto
+            aluno.update(mudancas)
             return aluno
     raise HTTPException(status_code=404, detail='Aluno não encontrado!')
 
